62f0e94d051bdd89f223b4a8b02875da8b177994
Juan Wajnerman <jwajnerman@manas.com.ar>
2017-09-12 15:46:01 -0300

    Initial commit

---
+++ lib/alto_guisso.ex
@@ -0,0 +1,18 @@
+defmodule AltoGuisso do
+  @moduledoc """
+  Documentation for AltoGuisso.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AltoGuisso.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/alto_guisso_test.exs
@@ -0,0 +1,8 @@
+defmodule AltoGuissoTest do
+  use ExUnit.Case
+  doctest AltoGuisso
+
+  test "greets the world" do
+    assert AltoGuisso.hello() == :world
+  end
+end
